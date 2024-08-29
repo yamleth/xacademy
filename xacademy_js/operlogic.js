e99aa83dde58ccdaddcf9c87764dce11ffa58f97
yamleth <yamarbell@gmail.com>
2024-08-29 20:51:53 -0300

    xacademy_js/

---
+++ xacademy_js/operlogic.js
@@ -0,0 +1,9 @@
+//aritmético
+let a = 5;
+let = 10;
+console.log(a)
+
+//otras operaciones
+let b = 5;
+ b+=10;
+console.log(b)
---
+++ xacademy_js/tipodeDatos.js
@@ -0,0 +1,36 @@
+//number
+
+let num1 = 4;
+let num2 = 5;
+console.log(typeof num2);
+
+//string
+
+let str = "esto es un string";
+let str2 = "esto es otro string";
+
+console.log(typeof str);
+
+//bigint
+
+let bignumber = 12533276374683764873648736487368n
+console.log(typeof bignumber);
+
+//boolean
+
+let bool1 = true;
+let bool2 = false;
+console.log(typeof bool2);
+
+//undefined
+
+let a;
+
+console.log(typeof a);
+// se puede declarar la variable aunque no seria una buena práctica
+//let a = 12
+//a = undefined
+
+
+//symbol
+//son utiles para definir propiedades en objetos aunque no lo utilizaremos a largo del curso
